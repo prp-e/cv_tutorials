e67d7329ec0a1801c592df1bcc2ac7c552679c46
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-08-31 21:02:45 +0430

    A simple pratcice

--- ep4/practice.py
+++ ep4/practice.py
@@ -8,6 +8,13 @@ for i in range(0,720):
     for j in range(0,1080):
         image[i,j] = (255,255,255)
 
+x, y = 270, 140
+w, h = 400, 400 
+
+cv2.rectangle(image, (x,y), (x+w, y+h), (200,155,0), 2)
+cv2.circle(image, (x, y), 20, (0,0,255), -1)
+cv2.circle(image, (x + w, y + h), 20, (0,0,255), -1)
+cv2.line(image, (x, y), (x + w, y + h), (0, 0, 0), 2)
 
 cv2.imshow('Canvas', image)
 cv2.waitKey(0)
